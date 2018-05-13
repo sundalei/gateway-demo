83f746ce3aa2a2460030bd87727581ead118fc3d
sundalei <sundalei@172.18.0.41>
2018-05-13 15:24:24 +0800

    initial commit

---
+++ ejbModule/packt/book/jee/eclipse/ch7/ejb/CourseBeanRemote.java
@@ -0,0 +1,13 @@
+package packt.book.jee.eclipse.ch7.ejb;
+
+import java.util.List;
+
+import javax.ejb.Remote;
+
+import packt.book.jee.eclipse.ch7.dto.CourseDTO;
+
+@Remote
+public interface CourseBeanRemote {
+	
+	public List<CourseDTO> getCourses();
+}
